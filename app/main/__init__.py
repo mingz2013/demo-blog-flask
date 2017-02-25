e82da356a4e2a3b7ac62549d7273a9dbdfc16c2f
mingz <305603665@qq.com>
2017-02-25 20:05:49 +0800

    init

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'zhaojm'
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 main = Blueprint('main', __name__, url_prefix='')
 
 from . import articles, errors
 
 
+@main.route('/test_post', methods=['POST'])
+def test_post():
+    print "test_post..."
+    print request.form
